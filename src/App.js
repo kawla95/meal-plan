e1e2317761d06efc5d6664e9528e9da2b75b3309
kawla95 <kawlamansur@hotmail.co.uk>
2022-06-16 16:10:42 +0100

    imported useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  return (
+    <div className="App">
+      <section className="controls"></section>
+    </div>
+  );
 }
 
 export default App;
