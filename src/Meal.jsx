36f9bf24dc4069cba6f1850862b1baa14376f9f1
kawla95 <kawlamansur@hotmail.co.uk>
2022-06-22 13:29:38 +0100

    added CSS

--- src/Meal.jsx
+++ src/Meal.jsx
@@ -19,7 +19,7 @@ export default function Meal({ meal }) {
     <article>
       <h1> {meal.title}</h1>
       <img src={imageUrl} alt="recipe"></img>
-      <ul>
+      <ul className="instructions">
         <li> Preparation time: {meal.readyInMinutes} minutes</li>
         <li> Number of servings: {meal.servings}</li>
       </ul>
